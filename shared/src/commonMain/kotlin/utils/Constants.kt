571540a51f83cda9be818b6ef3e1631c1931badf
Dhananjay-Navlani <164985893+Dhananjay-Navlani@users.noreply.github.com>
2024-07-29 20:32:44 +0530

    Update Constants.kt

--- shared/src/commonMain/kotlin/utils/Constants.kt
+++ shared/src/commonMain/kotlin/utils/Constants.kt
@@ -4,8 +4,10 @@ object Constants {
     const val SUPABASE_KEY = "your_supabase_key"
     const val SUPABASE_URL = "your_supabase_url"
     const val OAUTH_WEB_CLIENT_ID = "your_web_client_id_for_oauth"
+    //use this if using GeminiClient
     const val FLASH_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
     const val GEMINI_API_KEY = "your_gemini_api_key"
+    //default app is configured with OpenAiClient
     const val OPEN_LIBRARY_API_URL = "https://covers.openlibrary.org/b/isbn"
     const val OPENAI_CHAT_COMPLETION_URL = "https://api.openai.com/v1/chat/completions"
     const val OPENAI_API_KEY = "Bearer your_openai_key"
